a210dea92c48f95a4b8acc3cb3d4333347a9a0c6
ShereneC <spyhopper@cableone.net>
2021-10-06 22:33:57 -0600

    thru vid 13

--- app/main.js
+++ app/main.js
@@ -3,6 +3,17 @@ import express from "express";
 let server = express();
 let port = 3000;
 
+//this was in step 7, but not in the video
+//server.use(express.static(__dirname + '/public'));
+//this tells express that the front end of our website is in the folder 'public'
+
+server.get('/', (req, res, next) => {
+  res.send("Hello world")
+});
+server.get('/cats', (req, res, next) => {
+  res.send("<h1>Hello Cats</h1>")
+});
+
 server.listen(port, () => {
   console.log("server is running on port:", port);
-})
+});
