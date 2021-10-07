cab80b0a57d85d472062194ce578db62efa927d0
ShereneC <spyhopper@cableone.net>
2021-10-07 10:33:02 -0600

    thru vid 15

--- app/main.js
+++ app/main.js
@@ -1,18 +1,28 @@
 import express from "express";
+import bp from "body-parser";
 
 let server = express();
 let port = 3000;
 
-//this was in step 7, but not in the video
+let cats = [{ name: "wiskers" }, { name: "Scruffy" }]
+
+server.use(bp.json());
+
+//this was in step 7 text, but not in the video
 //server.use(express.static(__dirname + '/public'));
 //this tells express that the front end of our website is in the folder 'public'
 
 server.get('/', (req, res, next) => {
   res.send("Hello world")
-});
+})
 server.get('/cats', (req, res, next) => {
-  res.send("<h1>Hello Cats</h1>")
-});
+  res.send(cats)
+})
+server.post('/cats', (req, res, next) => {
+  req.body
+  cats.push(req.body)
+  res.send(cats)
+})
 
 server.listen(port, () => {
   console.log("server is running on port:", port);
