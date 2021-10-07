cfb2aa842a1ca6e5fe8802b767606da55e316344
ShereneC <spyhopper@cableone.net>
2021-10-07 10:53:01 -0600

    vid 17

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ let port = 3000;
 
 let cats = [{ name: "wiskers" }, { name: "Scruffy" }]
 
+// this registers the middleware that turns data into javascript objects (json)
 server.use(bp.json());
 
 //this was in step 7 text, but not in the video
@@ -19,9 +20,20 @@ server.get('/cats', (req, res, next) => {
   res.send(cats)
 })
 server.post('/cats', (req, res, next) => {
-  req.body
+  if (!req.body.name) {
+    return next("No Name Provided")
+    // the above sends an err with it so that it will then hit the default error handler because it will have 4 paramaters
+  }
   cats.push(req.body)
   res.send(cats)
+})
+//NOTE: Default Error Handler
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(400).send(err)
+})
+server.use((req, res, next) => {
+  res.status(404).send("Route not Found")
 })
 
 server.listen(port, () => {
